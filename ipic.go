9dd6ffeefc75c8af8240e48c19e3d4c1ef203576
Nathan Galt <adiabatic@users.noreply.github.com>
2019-08-15 01:11:39 -0700

    New year, new copyright

--- ipic.go
+++ ipic.go
@@ -1,4 +1,4 @@
-// © 2018 Nathan Galt
+// © 2019 Nathan Galt
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
